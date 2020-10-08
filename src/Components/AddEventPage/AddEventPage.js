52c89743bb4ad4cfe6053a3721a8d7ccb00683ef
azmir849 <azmir15-849@diu.edu.bd>
2020-10-08 13:33:29 +0600

    event method created and connect to database

--- src/Components/AddEventPage/AddEventPage.js
+++ src/Components/AddEventPage/AddEventPage.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddEventPage.css'
 import { Link } from 'react-router-dom';
 import VolunteerLOGO from '../../logos/Group 1329.png'
 const AddEventPage = () => {
+    const [event ,setEvent] = useState({
+        title:'',
+        date:'',
+        description:''
+    });
+    const handleBlur =(event)=>{
+        const newEventInfo ={...event};
+        newEventInfo[event.target.name] = event.target.value;
+        setEvent(newEventInfo);
+    }
+
+    const handleRegister =() =>{
+        const newEventInfo ={...event};
+        fetch('https://dry-meadow-25422.herokuapp.com/event',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(newEventInfo)
+            
+        })
+        window.alert("Event added successfully");
+    }
+
     return (
         <div>
             <div class="container">
@@ -16,13 +40,15 @@ const AddEventPage = () => {
                       <h6 className="listArea">Add event</h6>
                     </div>
                     <div className="eventPostArea">
+                    <form action="/event" method="POST">
                        <pre className="TextAreatitle">Event Title                        Event Date</pre>
-                        <input className="inputText" type="text" name="title" id="" placeholder="Enter title"/>
-                        <input type="date" name="title" id="" />
+                        <input onBlur={handleBlur} className="inputText" type="text" name="title" id="" placeholder="Enter title"/>
+                        <input onBlur={handleBlur} type="date" name="title" id="" />
                         <pre className="TextAreatitle">Description                          Banner</pre>
-                        <input className="inputText" type="text" name="" id="" placeholder="Enter Designation"/>
+                        <input onBlur={handleBlur} className="inputText" type="text" name="" id="" placeholder="Enter Designation"/>
                         <button className="btn btn-primary">Upload Image</button><br/><br/>
-                        <button className="SubmitButton btn btn-primary">Submit</button>
+                        <button  onClick={handleRegister} className="SubmitButton btn btn-primary">Submit</button>
+                        </form>
                     </div>
                 </div>
             </div>
